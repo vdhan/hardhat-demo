ed9937191751009424ccc89423f6b819f2032920
vdhan <anvudh@gmail.com>
2022-08-30 20:51:17 +0700

    configure for BSC

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,59 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+
+const { mnemonic, BscScan} = require('./env.json');
+
+task('accounts', 'Print list of accounts', async () => {
+  const accounts = await ethers.getSigners();
+
+  for (let account of accounts) {
+    console.log(account.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: 'mainnet',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545'
+    },
+
+    testnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {
+        mnemonic: mnemonic
+      }
+    },
+
+    mainnet: {
+      url: 'https://bsc-dataseed.binance.org/',
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {
+        mnemonic: mnemonic
+      }
+    }
+  },
+
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
+
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts'
+  },
+
+  mocha: {
+    timeout: 20000
+  }
 };
